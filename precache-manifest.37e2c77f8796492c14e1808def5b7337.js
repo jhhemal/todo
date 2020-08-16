f5d5bda395478a737f52a71e820d13e633e76027
Jahidul Hasan <jahidulhasanhemal@gmail.com>
2020-08-17 02:13:52 +0600

    Updates

---
+++ precache-manifest.37e2c77f8796492c14e1808def5b7337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398e3ba92dc0a727470093fdb8a98257",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "821822e1364c424d533a",
+    "url": "/todo/static/css/main.d085c223.chunk.css"
+  },
+  {
+    "revision": "e6c5f9755b0c348d7302",
+    "url": "/todo/static/js/2.78de81ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.78de81ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821822e1364c424d533a",
+    "url": "/todo/static/js/main.cac4cda4.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
