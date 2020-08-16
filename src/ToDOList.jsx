8be924c9a1c8b6c2042a618bcf25110fee42efc5
Jahidul Hasan <jahidulhasanhemal@gmail.com>
2020-08-17 02:19:26 +0600

    Updated readme file

--- src/ToDOList.jsx
+++ src/ToDOList.jsx
@@ -7,7 +7,6 @@ const ToDOList = (props) =>{
                 <i className="fa fa-times" aria-hidden="true" onClick={() => {
                     props.onSelect(props.id);
                 }}>
-
                 </i>
                 <li>{props.text}</li>
             </div>
